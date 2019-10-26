d272e911a7f6cfd1669fc7cfff82316441979082
Einar Rasmussen <einar@di.ku.dk>
2019-10-27 01:18:12 +0200

    Added print_symmem

--- interpreter/symbex/utils.ml
+++ interpreter/symbex/utils.ml
@@ -7,12 +7,6 @@ open Types
  * https://stackoverflow.com/a/53131220 *)
 let (+) a b = Int32.add a b
 
-(* replace with Module Option.Monad_infix
- * https://ocaml.janestreet.com/ocaml-core/latest/doc/base/Base/Option/Monad_infix/index.html
- * *)
-(* let (>>=) = bind (* Batteries.Option.bind *) *)
-
-
 (* Symbolic.hs/renderSym *)
 let rec render_sym (s : symword) : string =
     match s with
@@ -38,6 +32,20 @@ let print_symstack stackold =
     "[" ^ print_elems stack ^  "]"
 
 
+let print_symmem_entry key value : string =
+    "(" ^ Int32.to_string key ^ "->" ^ render_sym value ^ ")"
+
+let print_symmem symmem =
+    (*
+     * String.concat "\n"
+    ["";
+    "Memory Map";
+    "----------";
+    (String.concat "\n"
+    @@
+    *)
+    Store.fold (fun k v a -> print_symmem_entry k v ^ a) symmem ""
+
 
 let rec draw_tree ?(indent = 0l) (tree : symstate tree) =
     let indentation = String.make (Int32.to_int indent) ' ' in
@@ -47,9 +55,9 @@ let rec draw_tree ?(indent = 0l) (tree : symstate tree) =
     | Node (s, ss) -> let (ip, next, symmem, symstack, pc) = s in
                       print_indented @@ "IP: " ^ Int32.to_string ip;
                       print_indented @@ "Next: " ^ Int32.to_string next;
+                      print_indented @@ "SymMemory: " ^ print_symmem symmem;
                       print_indented @@ "SymStack: " ^ print_symstack symstack;
                       (*
-                      print_indented @@ "SymMemory: " ^ symmem;
                       print_indented @@ "Path Constraints: " ^ pc
                       *)
                       (* print_indented @@ "Solved Values" ^ ""*)
